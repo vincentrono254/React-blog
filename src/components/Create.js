be18f059e1186febf175f4e2fe4850cc001acd24
vincentrono254 <vincentmatson23@gmail.com>
2024-05-16 13:33:08 +0300

    saving data using json

--- src/components/Create.js
+++ src/components/Create.js
@@ -13,7 +13,10 @@
 
 // export default About;
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Create = () => {
   const [data, setData] = useState({
@@ -27,44 +30,78 @@ const Create = () => {
       return { ...prev, [name]: value };
     });
   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //check if all fields are filled
+    if (!data.title || !data.author || !data.body) {
+      toast.error("all fields must be filled", {
+        position: toast.POSITION.TOP_RiGHT,
+        autoClose: 3000,
+      });
+    }
+    axios
+      .post("http://localhost:4000/blogs", data)
+      .then((res) => {
+        toast.success("new blog added successfully", {
+          position: toast.POSITION.TOP_RiGHT,
+          autoClose: 3000,
+        });
+      })
+      .catch((error) => {
+        console.error("Error adding blog:", error);
+        toast.error(
+          "An error occurred while adding the blog. Please try again later.",
+          {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 3000,
+          }
+        );
+      });
+  };
   return (
     <div>
-      <Form.Group className="mb-3" controlId="exampleForm.controlInput">
-        <Form.Label>Title</Form.Label>
-        <Form.Control
-          name="title"
-          type="text"
-          Placeholder="Sample Title"
-          onChange={handleOnchange}
-          value={data.title}
-        ></Form.Control>
-      </Form.Group>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.controlInput">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            name="title"
+            type="text"
+            Placeholder="Sample Title"
+            onChange={handleOnchange}
+            value={data.title}
+          ></Form.Control>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="exampleForm.controlInput">
-        <Form.Label>Author</Form.Label>
-        <Form.Control
-          name="author"
-          type="text"
-          Placeholder="Sample Author"
-          onChange={handleOnchange}
-          value={data.author}
-        ></Form.Control>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.controlInput">
+          <Form.Label>Author</Form.Label>
+          <Form.Control
+            name="author"
+            type="text"
+            Placeholder="Sample Author"
+            onChange={handleOnchange}
+            value={data.author}
+          ></Form.Control>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="exampleForm.controlTextarea">
-        <Form.Label>Body</Form.Label>
-        <Form.Control
-          name="body"
-          as="textarea"
-          Placeholder="Sample Body"
-          rows={3}
-          onChange={handleOnchange}
-          value={data.body}
-        ></Form.Control>
-      </Form.Group>
-      <p>{data.title}</p>
+        <Form.Group className="mb-3" controlId="exampleForm.controlTextarea">
+          <Form.Label>Body</Form.Label>
+          <Form.Control
+            name="body"
+            as="textarea"
+            Placeholder="Sample Body"
+            rows={3}
+            onChange={handleOnchange}
+            value={data.body}
+          ></Form.Control>
+        </Form.Group>
+        <Button onClick={handleSubmit} variant="primary" type="submit">
+          <ToastContainer />
+          Submit
+        </Button>
+      </Form>
+      {/* <p>{data.title}</p>
       <p>{data.author}</p>
-      <p>{data.body}</p>
+      <p>{data.body}</p> */}
     </div>
   );
 };
